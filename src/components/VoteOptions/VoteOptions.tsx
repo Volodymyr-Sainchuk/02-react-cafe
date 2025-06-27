bca3c6ab705ca4836d65e435d777bf9bb6512a58
Volodymyr <sainchukvova2104@gmail.com>
2025-06-27 21:03:14 +0300

    do hw

---
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,29 @@
+import type { VoteType } from "../App/App";
+import css from "./VoteOptions.module.css";
+
+interface VoteOptionsProps {
+  onVote: (key: keyof VoteType) => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+
+export default function VoteOptions({ onVote, onReset, canReset }: VoteOptionsProps) {
+  return (
+    <div className={css.container}>
+      <button onClick={() => onVote("good")} className={css.button}>
+        Good
+      </button>
+      <button onClick={() => onVote("neutral")} className={css.button}>
+        Neutral
+      </button>
+      <button onClick={() => onVote("bad")} className={css.button}>
+        Bad
+      </button>
+      {canReset && (
+        <button onClick={onReset} className={`${css.button} ${css.reset}`}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
